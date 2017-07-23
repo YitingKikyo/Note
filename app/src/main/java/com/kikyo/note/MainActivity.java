08d6850c1f19df33069fa5649b7b4a4fe9a3f2d0
unknown <987980045@qq.com>
2017-07-24 00:47:27 +0800

    drawer layout recycle list and alert dialog

--- app/src/main/java/com/kikyo/note/MainActivity.java
+++ app/src/main/java/com/kikyo/note/MainActivity.java
@@ -1,25 +1,20 @@
 package com.kikyo.note;
 
-import android.app.Activity;
-import android.app.Fragment;
-import android.app.FragmentManager;
-import android.media.Image;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.LinearLayout;
-import android.widget.ListView;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -29,13 +24,13 @@ import com.kikyo.note.service.FileService;
 import com.kikyo.note.service.NoteService;
 import com.yqritc.recyclerviewflexibledivider.HorizontalDividerItemDecoration;
 
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
+
 
 public class MainActivity extends AppCompatActivity  {
 
+    private static final String LOG_TAG = "MainActivity";
     private Toolbar mToolbar;
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mDrawerToggle;
@@ -51,7 +46,6 @@ public class MainActivity extends AppCompatActivity  {
     //colorPrimaryDark不是用來做背景色的~他是狀態欄的眼色
     //Toolbar包括上面一栏的全部东西，比如标题，菜单图标, 搜索图标都是Toolbar的内容，不用自己用TextView, ImageView
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -61,9 +55,13 @@ public class MainActivity extends AppCompatActivity  {
 
     }
 
+
     private void setUpViews() {
         View view = View.inflate(this, R.layout.activity_main, null);
+        //这里已经有一个set***了
         setContentView(view);
+
+        setBottleButtonLis();
 
         setUpToolbar();
         setUpDrawer();
@@ -99,7 +97,6 @@ public class MainActivity extends AppCompatActivity  {
             //在数据库加两个测试~
             mNoteService.insertNote(new Note("啦啦啦", "嘿嘿嘿"));
             mNoteService.insertNote(new Note("略略", "嘤嘤嘤"));
-            //我们以他返回的列表新创建一个列表，新的列表就是可以修改的。额..感觉讲的不明不白
             mNotes = new ArrayList<>(mNoteService.getAllNotss());
         }
         mNoteList.setAdapter(new NoteListAdapter(mNotes));
@@ -147,7 +144,6 @@ public class MainActivity extends AppCompatActivity  {
             holder.summary.setText(note.getContent());
             holder.date.setText(note.getDate());
             holder.first.setText(note.getTitle().substring(0, 1));
-            //這裏用了他的
         }
 
 
@@ -168,7 +164,6 @@ public class MainActivity extends AppCompatActivity  {
                 @Override
                 public void onClick(View view) {
                     int pos = getAdapterPosition();
-                    Toast.makeText(view.getContext(), "点击了" + pos, Toast.LENGTH_SHORT).show();
                     //他的异常是Collections$UnmodifiableList，不可修改的列表。
                     //这时因为查询操作返回的列表是不可修改的。呃，感觉这种异常要有经验才能一看就知道是上面。
                     Note removedNote = mNotes.remove(pos);
@@ -192,17 +187,11 @@ public class MainActivity extends AppCompatActivity  {
             //在数据库加两个测试~
             mFileService.insertFile(new File("嘿嘿文件", 2));
             mFileService.insertFile(new File("略略文件", 1));
-            //我们以他返回的列表新创建一个列表，新的列表就是可以修改的。额..感觉讲的不明不白
             mFiles = new ArrayList<>(mFileService.getAllFiles());
         }
         mFileList.setAdapter(new FileListAdapter(mFiles));
-        mFileList.addItemDecoration(new HorizontalDividerItemDecoration.Builder(this)
-                .color(R.color.divider)
-                .sizeResId(R.dimen.divider)
-                .build());
     }
 
-    //这里的泛型参数是FileViewHolder，下面的onCreateViewHolder的参数也应该是FileViewHolder
     private class FileListAdapter extends RecyclerView.Adapter<FileViewHolder> {
 
         private List<File> mFiles;
@@ -219,15 +208,11 @@ public class MainActivity extends AppCompatActivity  {
         @Override
         public void onBindViewHolder(FileViewHolder holder, int position) {
             File file = mFiles.get(position);
-            //我记得你当时是这样写的。然后，关键问题是导错包了，就是import了java.io.File，然后你就找不到那个函数了。
-            //同名的类比较容易出现这种差错~。记得导包的时候看一下~一般代码提示的时候，发现函数不是自己的那些，就知道是导错了
             holder.file_name.setText(file.getFileName());
-            holder.file_num.setText("2");
+            holder.file_num.setText(String.valueOf(file.getFileNum()));
         }
 
 
-
-        //复制代码的时候很容易出现这种问题。忘了改一些地方~所以复制代码要小心一点。
         @Override
         public int getItemCount() {
             return mFiles.size();
@@ -238,19 +223,74 @@ public class MainActivity extends AppCompatActivity  {
 
         public TextView file_name, file_num;
 
-        public FileViewHolder(View itemView) {
+        public FileViewHolder(final View itemView) {
+
             super(itemView);
-            itemView.setOnClickListener(new View.OnClickListener() {
+
+            itemView.setOnLongClickListener(new View.OnLongClickListener() {
+
                 @Override
-                public void onClick(View view) {
-                    int pos = getAdapterPosition();
-                    Toast.makeText(view.getContext(), "点击了" + pos, Toast.LENGTH_SHORT).show();
-                    //他的异常是Collections$UnmodifiableList，不可修改的列表。
-                    //这时因为查询操作返回的列表是不可修改的。呃，感觉这种异常要有经验才能一看就知道是上面。
-                    File removedFile = mFiles.remove(pos);
-                    mFileService.deleteFile(removedFile);
-                    //导错包了[笑cry]。java里有一个File类，和你自己写的重复了，import的时候弄错了
-                    mFileList.getAdapter().notifyItemRemoved(pos);
+                public boolean onLongClick(View v) {
+
+                    final int pos = getAdapterPosition();
+                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                    //通过布局填充器获login_layout
+                    View view = getLayoutInflater().inflate(R.layout.rename_or_delete_dia,null);
+
+                    TextView t1 = (TextView) view.findViewById(R.id.renameFile);
+                    TextView t2 = (TextView) view.findViewById(R.id.deleteFile);
+
+                    builder.setView(view);//设置login_layout为对话提示框
+                    final AlertDialog dialog = builder.show(); //显示Dialog对话框
+
+                    t1.setOnClickListener(new View.OnClickListener(){
+                        @Override
+                        public void onClick(View v) {
+                            AlertDialog.Builder builder1 = new AlertDialog.Builder(MainActivity.this);
+                            //通过布局填充器获login_layout
+                            View view1 = getLayoutInflater().inflate(R.layout.rename_file_dia,null);
+
+                            final EditText newFileName1 = (EditText) view1.findViewById(R.id.new_rename_file_name);
+                            newFileName1.setText( mFiles.get(pos).getFileName());
+
+                            TextView tt1 = (TextView) view1.findViewById(R.id.sure_rename);
+                            TextView tt2 = (TextView) view1.findViewById(R.id.cancel_rename);
+
+                            //id 弄错了，取消的id变成ensure_to_create了..倒错了
+                            builder1.setView(view1);//设置login_layout为对话提示框
+                            final AlertDialog dialog1 = builder1.show(); //显示Dialog对话框
+
+                            tt1.setOnClickListener(new View.OnClickListener(){
+                                @Override
+                                public void onClick(View v) {
+                                    //现在这个报错的意思是，执行了EditText的getText()，但是这个EditText为null。也就是newFileFile为null。
+                                    String a = newFileName1.getText().toString().trim();
+                                    File f = new File(a, mFiles.get(pos).getFileNum());
+                                    mFileList.getAdapter().notifyItemChanged(pos, f);
+                                    dialog1.dismiss();
+                                    dialog.dismiss();
+                                }
+                            });
+                            tt2.setOnClickListener(new View.OnClickListener(){
+                                @Override
+                                public void onClick(View v) {
+                                    dialog1.dismiss();
+                                    dialog.dismiss();
+                                }
+                            });
+                        }
+                    });
+                    t2.setOnClickListener(new View.OnClickListener(){
+                        @Override
+                        public void onClick(View v) {
+                            File removedFile = mFiles.remove(pos);
+                            mFileService.deleteFile(removedFile);
+                            mFileList.getAdapter().notifyItemRemoved(pos);
+                            dialog.dismiss();
+                        }
+                    });
+
+                    return true;
                 }
             });
             file_name = (TextView) itemView.findViewById(R.id.file_name);
@@ -259,5 +299,60 @@ public class MainActivity extends AppCompatActivity  {
         }
     }
 
+    public void setBottleButtonLis() {
+        //点击效果我们一般是点击一整列都会弹出对话框，而不是只能点击那个图标~
+        findViewById(R.id.new_file).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //打个日志以确定这个函数揹执行了，从而确定问题是OnClickListener没有用还是下面的buildNewFileDiaShow没有用。
+                Log.d(LOG_TAG, "new file button clicked!");
+                buildNewFileDiaShow();
+            }
+        });
+    }
+
+    public void buildNewFileDiaShow(){
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        //通过布局填充器获login_layout
+        View view = getLayoutInflater().inflate(R.layout.new_file_dia,null);
+
+        final EditText newFileName = (EditText) view.findViewById(R.id.new_file_name);
+
+        TextView t1 = (TextView) view.findViewById(R.id.ok);
+        TextView t2 = (TextView) view.findViewById(R.id.cancel);
+
+        //id 弄错了，取消的id变成ensure_to_create了..倒错了
+        builder.setView(view);//设置login_layout为对话提示框
+        final AlertDialog dialog = builder.show(); //显示Dialog对话框
+
+        t1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Log.d(LOG_TAG, "ok clicked");
+                //现在这个报错的意思是，执行了EditText的getText()，但是这个EditText为null。也就是newFileFile为null。
+                String a = newFileName.getText().toString().trim();
+
+                File f = new File(a, 0);
+                //新建文件夹
+                boolean success = mFileService.insertFile(f);
+                if(success) {
+                    //这里增加了一个File，重新设置Adapter的話效率低，直接通知他有新文件就好了
+                    mFiles.add(f);
+                    mFileList.getAdapter().notifyItemInserted(mFiles.size() - 1);
+                }
+                dialog.dismiss();
+                Toast.makeText(MainActivity.this, success ? R.string.create_successfully : R.string.creat_failed, Toast.LENGTH_SHORT).show();
+            }
+        });
+        t2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Log.d(LOG_TAG, "cancel clicked");
+                dialog.dismiss();
+            }
+        });
+
+    }
 
 }
